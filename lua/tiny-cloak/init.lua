fe0fa357d9ba137cd1e8f8cea440e383893f8fd5
Huynh Duc Dung <dunghd.it@gmail.com>
2026-01-19 20:41:08 +0800

    feat: US-002 - Implement core cloaking engine with extmarks

--- lua/tiny-cloak/init.lua
+++ lua/tiny-cloak/init.lua
@@ -5,6 +5,21 @@ local default_config = {
   cloak_character = '*',
 }
 
+local namespace = vim.api.nvim_create_namespace('tiny-cloak')
+
+function M.cloak_line(bufnr, line_num, start_col, end_col)
+  local config = M.config
+  local line_length = end_col - start_col
+  local cloak_char = config.cloak_character
+  local cloak_text = string.rep(cloak_char, line_length)
+
+  vim.api.nvim_buf_set_extmark(bufnr, namespace, line_num, start_col, {
+    end_col = end_col,
+    virt_text = { { cloak_text, 'Comment' } },
+    virt_text_pos = 'overlay',
+  })
+end
+
 function M.setup(opts)
   opts = opts or {}
   local config = vim.tbl_deep_extend('force', default_config, opts)
